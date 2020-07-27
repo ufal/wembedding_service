0ce129306c309d805ead323a618816488e9eb962
Jana Strakova <strakova@ufal.mff.cuni.cz>
2020-07-27 10:43:56 +0200

    WEmbeddings client can be imported also as module.

--- wembeddings_client.py
+++ wembeddings_client.py
@@ -10,16 +10,16 @@
 
 """Word embeddings client.
 
-This client computes word embeddings either locally by importing the
-WEmbeddings class as a module or by sending requests to a running WEmbeddings
-server (see wembeddings_server.py) indentified by a host:port pair.
+WEmbeddings client computes word embeddings either locally or by sending
+requests to a running WEmbeddings server (see wembeddings_server.py).
 
-The client accepts either an input file or reads data from standard input.
+The client can be used as a script or imported as a module.
 
-It prints out results to standard output.
+The script accepts either an input file or reads data from standard input. It
+prints out results to standard output.
 
-Commandline arguments:
-----------------------
+Script commandline arguments:
+-----------------------------
     
     --host: Either host:port pair or None for local processing. Default:
             None
@@ -54,8 +54,61 @@ John
 .
 """
 
-
+import json
+import pickle
+import requests
 import sys
+
+
+class WEmbeddingsClient:
+    def __init__(self, host, batch_size):
+        self._host = host
+        self._batch_size = batch_size
+
+
+    def _make_post_request(self, model, sentences):
+        response = requests.post(self._host,
+                                 json.JSONEncoder().encode({"model": model,
+                                                           "sentences": sentences}))
+        if response.ok:
+            print("Successfully processed request, time elapsed: {}".format(response.elapsed), file=sys.stderr, flush=True)
+            return pickle.loads(response.content)
+        else:
+            print("A server error occured: Response status code = {}".format(response.status_code), file=sys.stderr, flush=True)
+            sys.exit(1)
+        
+
+    def compute_embeddings(self, model, sentences):
+        if self._host:
+            print("Sending requests to {}".format(self._host), file=sys.stderr, flush=True)
+
+            outputs = []
+            batch = []
+            for sentence in sentences:
+                if len(batch) == self._batch_size:
+                    outputs.extend(self._make_post_request(model, batch))
+                    batch = []
+                batch.append(sentence)
+            if batch:
+                outputs.extend(self._make_post_request(model, batch))
+            return outputs
+
+        else:
+            print("Computing word embeddings locally.", file=sys.stderr, flush=True)
+
+            import wembeddings
+
+            wembeddings = wembeddings.WEmbeddings()
+            outputs = []
+            batch = []
+            for sentence in sentences:
+                if len(batch) == self._batch_size:
+                    outputs.extend(wembeddings.compute_embeddings(model, batch))
+                    batch = []
+                batch.append(sentence)
+            if batch:
+                outputs.extend(wembeddings.compute_embeddings(model, batch))
+            return outputs
 
 
 if __name__ == "__main__":
@@ -63,79 +116,30 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", default=None, type=str, help="host:port or None to process locally")
+    parser.add_argument("--host", default=None, type=str, help="ip:port or None")
     parser.add_argument("--infile", default=sys.stdin, type=argparse.FileType("r"), help="path to file or None for stdio")
     parser.add_argument("--model", default="bert-base-multilingual-uncased-last4", type=str, help="Model name (see wembeddings.py for options)")
     parser.add_argument("--batch_size", default=64, type=int, help="Batch size (maximum number of sentences per batch)")
     args = parser.parse_args()
 
     # Read sentences
-    sentences = []  # batches x sentences x words
-    batch = []      # current batch: sentences x words
-    sentence = []   # current sentence
-    nsentences = 0
+    sentences = []  
+    sentence = []  
     for line in args.infile:
         word = line.rstrip()
         if word:
             sentence.append(word)
         else:
-            if len(batch) == args.batch_size:
-                sentences.append(batch)
-                batch = []
-            batch.append(sentence)
-            nsentences += 1
+            sentences.append(sentence)
             sentence = []
-
-    # Leftover sentence (file not properly ended with empty line)
     if sentence:
-        if len(batch) == args.batch_size:
-            sentences.append(batch)
-            batch = []
-        batch.append(sentence)
-        nsentences += 1
-
-    # Leftover batch
-    if batch:
-        sentences.append(batch)
-
-    # Close infile
+        sentences.append(sentence)
     args.infile.close()
-
-    print("Read {} sentences in {} batches.".format(nsentences, len(sentences)), file=sys.stderr, flush=True)
+    print("Read {} sentences.".format(len(sentences)), file=sys.stderr, flush=True)
 
     # Compute word embeddings
-    if args.host:
-        print("Sending requests to {}".format(args.host), file=sys.stderr, flush=True)
-
-        import json
-        import pickle
-        import requests
-
-        # Process batches by sending requests to server
-        outputs = []
-        for i, batch in enumerate(sentences):
-            print("Computing batch {} of {}.".format(i+1, len(sentences)), file=sys.stderr, flush=True)
-            response = requests.post(args.host,
-                                     json.JSONEncoder().encode({"model": args.model,
-                                                                "sentences": batch}))
-            if response.ok:
-                print("Successfully processed request, time elapsed: {}".format(response.elapsed), file=sys.stderr, flush=True)
-                outputs.extend(pickle.loads(response.content))
-            else:
-                print("A server error occured: Response status code = {}".format(response.status_code), file=sys.stderr, flush=True)
-                sys.exit(1)
-
-    else:
-        print("Computing word embeddings locally.", file=sys.stderr, flush=True)
-
-        import wembeddings
-
-        # Process batches locally
-        wembeddings = wembeddings.WEmbeddings()
-        outputs = []
-        for i, batch in enumerate(sentences):
-            print("Computing batch {} of {}.".format(i+1, len(sentences)), file=sys.stderr, flush=True)
-            outputs.extend(wembeddings.compute_embeddings(args.model, batch))
+    client = WEmbeddingsClient(args.host, args.batch_size)
+    outputs = client.compute_embeddings(args.model, sentences)
 
     # Print outputs
     for sentence_output in outputs:
