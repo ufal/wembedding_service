3a1062934626286320b54b669a95bbfeb532d3f3
Milan Straka <milan@strakovi.com>
2020-10-01 12:54:46 +0200

    Only compute embeddings when there is at least 1 sentence.

--- wembeddings/wembeddings.py
+++ wembeddings/wembeddings.py
@@ -102,58 +102,59 @@ class WEmbeddings:
         if model not in self._models:
             print("No such WEmbeddings model {}".format(model), file=sys.stderr, flush=True)
 
-        model = self._models[model]
-        model.load()
-
-        time_tokenization = time.time()
-
-        subwords, segments, parts = [], [], []
-        for i, sentence in enumerate(sentences):
-            segments.append([])
-            subwords.append([])
-            parts.append([0])
-            for word in sentence:
-                word_subwords = model.tokenizer.encode(word[:self._max_form_len], add_special_tokens=False)
-                # Split sentences with too many subwords
-                if len(subwords[-1]) + len(word_subwords) > self.MAX_SUBWORDS_PER_SENTENCE:
-                    subwords[-1] = model.tokenizer.build_inputs_with_special_tokens(subwords[-1])
-                    segments.append([])
-                    subwords.append([])
-                    parts[-1].append(0)
-                segments[-1].extend([parts[-1][-1]] * len(word_subwords))
-                subwords[-1].extend(word_subwords)
-                parts[-1][-1] += 1
-            subwords[-1] = model.tokenizer.build_inputs_with_special_tokens(subwords[-1])
-
-        max_sentence_len = max(len(sentence) for sentence in sentences)
-        max_subwords = max(len(sentence) for sentence in subwords)
-
-        time_embeddings = time.time()
-        np_subwords = np.zeros([len(subwords), max_subwords], np.int32)
-        for i, subword in enumerate(subwords):
-            np_subwords[i, :len(subword)] = subword
-
-        np_segments = np.full([len(segments), max_subwords - 1], max_sentence_len, np.int32)
-        for i, segment in enumerate(segments):
-            np_segments[i, :len(segment)] = segment
-
-        embeddings_with_parts = model.compute_embeddings(np_subwords, np_segments).numpy()
-
-        # Concatenate splitted sentences
         embeddings = []
-        current_sentence_part = 0
-        for sentence_parts in parts:
-            embeddings.append(np.concatenate(
-                [embeddings_with_parts[current_sentence_part + i, :sentence_part] for i, sentence_part in enumerate(sentence_parts)],
-                axis=0))
-            current_sentence_part += len(sentence_parts)
+        if sentences:
+            model = self._models[model]
+            model.load()
 
-        print("WEmbeddings in {:.1f}ms,".format(1000 * (time.time() - time_embeddings)),
-              "tokenization in {:.1f}ms,".format(1000*(time_embeddings - time_tokenization)),
-              "batch {},".format(len(sentences)),
-              "max sentence len {},".format(max_sentence_len),
-              "max subwords {}.".format(max_subwords),
-              file=sys.stderr, flush=True)
+            time_tokenization = time.time()
+
+            subwords, segments, parts = [], [], []
+            for i, sentence in enumerate(sentences):
+                segments.append([])
+                subwords.append([])
+                parts.append([0])
+                for word in sentence:
+                    word_subwords = model.tokenizer.encode(word[:self._max_form_len], add_special_tokens=False)
+                    # Split sentences with too many subwords
+                    if len(subwords[-1]) + len(word_subwords) > self.MAX_SUBWORDS_PER_SENTENCE:
+                        subwords[-1] = model.tokenizer.build_inputs_with_special_tokens(subwords[-1])
+                        segments.append([])
+                        subwords.append([])
+                        parts[-1].append(0)
+                    segments[-1].extend([parts[-1][-1]] * len(word_subwords))
+                    subwords[-1].extend(word_subwords)
+                    parts[-1][-1] += 1
+                subwords[-1] = model.tokenizer.build_inputs_with_special_tokens(subwords[-1])
+
+            max_sentence_len = max(len(sentence) for sentence in sentences)
+            max_subwords = max(len(sentence) for sentence in subwords)
+
+            time_embeddings = time.time()
+            np_subwords = np.zeros([len(subwords), max_subwords], np.int32)
+            for i, subword in enumerate(subwords):
+                np_subwords[i, :len(subword)] = subword
+
+            np_segments = np.full([len(segments), max_subwords - 1], max_sentence_len, np.int32)
+            for i, segment in enumerate(segments):
+                np_segments[i, :len(segment)] = segment
+
+            embeddings_with_parts = model.compute_embeddings(np_subwords, np_segments).numpy()
+
+            # Concatenate splitted sentences
+            current_sentence_part = 0
+            for sentence_parts in parts:
+                embeddings.append(np.concatenate(
+                    [embeddings_with_parts[current_sentence_part + i, :sentence_part] for i, sentence_part in enumerate(sentence_parts)],
+                    axis=0))
+                current_sentence_part += len(sentence_parts)
+
+            print("WEmbeddings in {:.1f}ms,".format(1000 * (time.time() - time_embeddings)),
+                  "tokenization in {:.1f}ms,".format(1000*(time_embeddings - time_tokenization)),
+                  "batch {},".format(len(sentences)),
+                  "max sentence len {},".format(max_sentence_len),
+                  "max subwords {}.".format(max_subwords),
+                  file=sys.stderr, flush=True)
 
         return embeddings
 
